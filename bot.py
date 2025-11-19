49a49bdf71a8e1c94e6c665cb223a8538208204b
Wallace Lee <wallace.lee0412@gmail.com>
2025-11-19 17:32:20 -0500

    Adding error

--- bot.py
+++ bot.py
@@ -339,6 +339,7 @@ async def on_message(message):
         if message.author.id not in tracked_users:
             # Check if user has the "Dailies Challenger" role
             member = message.guild.get_member(message.author.id)
+            print(member.roles)
             if any(role.name.lower() == "dailies challenger" for role in member.roles):
                 # Automatically add user to tracking if they have the "Dailies Challenger" role
                 user_nickname = member.nick if member and member.nick else message.author.name
@@ -355,7 +356,7 @@ async def on_message(message):
                     'revival': 0,
                     'buffer': 0,
                     'probation': False,
-                    'ping': False,  # Enable pings by default for auto-tracked users
+                    'ping': False,  
                     'duels_won': 0,
                     'duels_lost': 0
                 }
